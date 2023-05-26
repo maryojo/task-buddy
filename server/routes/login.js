00da9165471d30655ac6d806be45c14a5e463ae0
Mary Ojo <maryojo3@gmail.com>
2023-05-26 15:00:01 +0100

    Login authentication

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-const {User} = require('../models');
+const { User } = require("../models");
+// const { generateToken} = require('../authentication/jwtUtils');
 
+const secretKey = process.env.SECRET_KEY;
 
-const secretKey = process.env.SECRET_KEY; 
-
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const isValid = validateCredentials(email, password);
-
-    if (!isValid) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+    const isValid = await validateCredentials(email, password);
+    if (isValid === false) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // Generate JWT token
-    const token = jwt.sign({ email }, secretKey, { expiresIn: '1h' });
-    res.status(200).json({ token });
+    {
+      const token = jwt.sign({ email }, secretKey, { expiresIn: "1h" });
+      res.status(200).json({ token });
+    }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
@@ -32,7 +32,7 @@ async function validateCredentials(email, password) {
       where: { email, password },
     });
 
-    return !!user;
+    return user !== null;
   } catch (error) {
     console.error(error);
     return false;

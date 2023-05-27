4eacabcb3673ae5ff2a7d87adbabfa4e0d6691f1
Mary Ojo <maryojo3@gmail.com>
2023-05-27 01:44:36 +0100

    Frontend authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
-
+import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Register from "./pages/register";
+import NotFound from "./pages/not-found";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route exact path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </Router>
+      <ToastContainer />
     </>
   );
 }

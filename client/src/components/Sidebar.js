c882c9bbf6e721713090a24cd799d7260a30dae9
Mary Ojo <maryojo3@gmail.com>
2023-05-27 03:52:47 +0100

    Added logout function

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SideCard from '../components/SideCard'
 import ProfileCard from './ProfileCard'
+import LogoutButton from './LogoutButton'
 
 const Sidebar = () => {
   return (
     <div className='w-3/12 bg-slate-300 rounded-md'>
       <ProfileCard/>
       <SideCard/>
+      <LogoutButton/>
     </div>
   )
 }

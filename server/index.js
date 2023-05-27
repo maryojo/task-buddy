1c501cf6d0729aa2b5546ee7aecfc4e4bcdc4ddd
Mary Ojo <maryojo3@gmail.com>
2023-05-27 02:49:22 +0100

    Changes to server index

--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
 require('dotenv').config({path:'./.env'});
 
 const express = require('express');
+const cors = require('cors');
 const apiRoutes = require('./routes/routes');
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
 const { sequelize, connectToDatabase} = require('./database');
 
-
 const app = express();
-const PORT = 3001;
-
-
 app.use(express.json());
+
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+app.use(cors(corsOptions));
+
 
 //Routes
 app.use('/api/login', loginRouter);
 app.use('/api/register', registerRouter);
 app.use('/api', apiRoutes);
 
+const PORT = 3001;
 
 app.listen(PORT, async () =>{
   console.log('listening on port: '+PORT);
-  console.log(process.env.PORT);
   await connectToDatabase();
 })
